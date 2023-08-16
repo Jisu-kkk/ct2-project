d2e0e518ce3e42a7be00d1f8b72fd61d0b74da94
[이모션]강지수 <jisu.kang@emotion.co.kr>
2023-08-16 12:13:51 +0900

    admin 추가

---
+++ src/main/java/com/example/ct2/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.example.ct2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/wiki")
+    public String wiki() {
+        return "admin/wiki";
+    }
+}
